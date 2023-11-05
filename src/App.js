854a6cbacd87f37fc9676f786aee62cce1aae429
leoluffy0105 <leoluffy0105@gmail.com>
2023-11-05 14:33:43 +0100

    change some issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Album from "./pages/Album";
 import Users from "./pages/users";
-
-const Contact = () => {
-  return <>Contact</>;
-};
 
 const App = () => {
   return (
--- src/pages/users.js
+++ src/pages/users.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./users.css";
-import Album from "./Album";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
